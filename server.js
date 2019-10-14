bb40960912cfb323dc8bec2f5de5201f5d4fc581
Beguwala <rbeguwala@dxc.com>
2019-10-14 15:47:34 +0530

    ready to deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express= require("express");
 const app= express();
 const mongoose=require("mongoose");
 const config= require("config")
+const path=require("path")
 
 const db="mongodb://localhost:27017/college_insights";
 mongoose
@@ -10,6 +11,15 @@ mongoose
 .catch((err)=>console.log(err));
 
 app.use(express.json({extended:false}))
+
+if(process.env.NODE_ENV=='production'){
+    //set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 app.use("/student",require("./routes/api/student"))
 
