717254b9f9867f651559556f34c8ec992648faea
Ruqaiya <Ruqaiya.beguwala@gmail.com>
2020-07-09 19:51:07 +0530

    setting attendance and adding login features

---
+++ client/src/setupProxy.js
@@ -0,0 +1,7 @@
+const proxy = require('http-proxy-middleware');
+ 
+module.exports = function(app) {
+    app.use(proxy('/auth/**', { target: 'http://localhost:5000' }));
+    app.use(proxy('/student/**', { target: 'http://localhost:5000' }));
+    app.use(proxy('/search/**', { target: 'http://localhost:5000' }));
+};
